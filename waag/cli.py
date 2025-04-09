69e74564beec87297e483a7c4e504314fb898bf9
Natanael Arndt <arndtn@gmail.com>
2025-04-09 17:06:02 +0200

    Adjust interface

--- waag/cli.py
+++ waag/cli.py
@@ -1,19 +1,28 @@
 import click
+import sys
 
 from .list_parser import ListParser
-from .graph_generation import GraphGeneration
 
 
 @click.command()
-@click.option("--file", help="The location of the awesome list (Markdown).")
+@click.option(
+    "--awesome-file", "-a", help="The location of the awesome list (Markdown)."
+)
+@click.option(
+    "--graph-output", "--output", "-o", help="Where to write the graph to.", default="-"
+)
 @click.option("--base-iri", "--iri", help="The base IRI of the document.")
-def cli(file, base_iri):
+def cli(awesome_file, base_iri, graph_output):
     """Get the urls from the markdown files"""
-    parser = ListParser(file, base_iri)
+
+    if graph_output == "-":
+        graph_output = sys.stdout
+
+    parser = ListParser(awesome_file, base_iri)
     parser.parse()
     print("===========")
-    # parser.get_concepts()
-    parser.get_projects()
+    parser.get_concepts()
+    # parser.get_projects()
     # graph_generation = GraphGeneration()
     # graph = graph_generation(parser)
 
