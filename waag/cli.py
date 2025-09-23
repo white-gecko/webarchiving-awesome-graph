bf5df7c19c4b2ecfda1758503915f6b45562ea3b
Natanael Arndt <arndtn@gmail.com>
2025-09-23 16:57:34 +0200

    Remove unused cli option

--- waag/cli.py
+++ waag/cli.py
@@ -10,17 +10,11 @@ from .graph_generation import GraphGeneration
     "--awesome-file", "-a", help="The location of the awesome list (Markdown)."
 )
 @click.option(
-    "--graph-output", "--output", "-o", help="Where to write the graph to.", default="-"
-)
-@click.option(
     "--output-dir", help="Where to write the individual graphs to.", default="graphs"
 )
 @click.option("--base-iri", "--iri", help="The base IRI of the document.")
-def cli(awesome_file, base_iri, graph_output, output_dir):
+def cli(awesome_file, base_iri, output_dir):
     """Get the urls from the markdown files"""
-
-    if graph_output == "-":
-        graph_output = sys.stdout
 
     graph_generation = GraphGeneration(base_iri=base_iri)
     graph_generation.init_from_dir(Path(output_dir))
