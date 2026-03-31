ba5609193609dd3dc5880e1136590683ddf3dc43
Natanael Arndt <arndtn@gmail.com>
2026-03-31 13:57:33 +0200

    rename project resources to common subject iri

--- waag/graph_generation.py
+++ waag/graph_generation.py
@@ -26,7 +26,7 @@ class GraphGeneration:
     def fetch_doap(self):
         if not self.doap_graph:
             self.doap_graph = Graph()
-            for project in self.projects():
+            for _, project in self.projects():
                 doap = doapit(project)
                 if doap:
                     self.doap_graph += doap
@@ -36,7 +36,7 @@ class GraphGeneration:
         """Get all projects.
         Yields a tuple of a project's subject IRI and optionally the respective URL of the project on an online platform (like GitHub)."""
         for row in self.graph.query(queries.list_projects()):
-            yield row.project
+            yield row.project, row.platform_project
 
     def write_to_graph(self, graph_path: Path):
         with open(graph_path, mode="wb") as file:
@@ -61,6 +61,8 @@ class GraphGeneration:
             self.doap_graph.parse(doap)
 
     def get_awesome_graph(self):
+        for project, platform_project in self.projects():
+            self.rename_resource(self.doap_graph, platform_project, project)
         return self.graph + self.doap_graph
 
     def store_to_dir(self, readme: Path, output_dir: Path):
@@ -71,3 +73,15 @@ class GraphGeneration:
         call_and_write(self.parser.get_readme_graph, output_dir / "readme.ttl")
         call_and_write(self.fetch_doap, output_dir / "doap.ttl")
         call_and_write(self.get_awesome_graph, output_dir / "awesome.ttl")
+
+    def rename_resource(self, graph: Graph, resource_from: URIRef, resource_to: URIRef):
+        """Move all triples from a subject `resource_from` to a new subject `resource_to`.
+        This will merge all triples ?s_from ?p ?o with ?s_to ?p ?o to ?s_to ?p ?o .
+
+        This method will alter the provided graph."""
+
+        for resource_from, p, o in graph.triples((resource_from, None, None)):
+            graph.add((resource_to, p, o))
+            graph.remove((resource_from, p, o))
+
+        return graph
